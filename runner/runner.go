0da7dc4e00e0f52ea00d66310bbe6a373eaaaf21
Everton H. Taques <97463920+etaques@users.noreply.github.com>
2023-05-10 09:23:51 -0300

    Update runner.go

--- runner/runner.go
+++ runner/runner.go
@@ -13,7 +13,7 @@ import (
 	"time"
 
 	"github.com/amenzhinsky/go-memexec"
-	"github.com/leoparente/otlpinf/config"
+	"github.com/leoparente/opentelemetry-infinity/config"
 	"go.uber.org/zap"
 	"gopkg.in/yaml.v3"
 )
